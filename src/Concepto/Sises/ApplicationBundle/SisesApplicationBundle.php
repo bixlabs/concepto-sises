274fa5fc02c3682351bb8cbd617c3b8fec8961b4
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-08-28 20:27:34 -0500

    close source license

--- src/Concepto/Sises/ApplicationBundle/SisesApplicationBundle.php
+++ src/Concepto/Sises/ApplicationBundle/SisesApplicationBundle.php
@@ -22,11 +22,21 @@
  * THE SOFTWARE.
  */
 
-namespace Concepto\Sises\ApplicationBundle;
+ /**
+ * Copyright © 2014 Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
+ *
+ * This file is part of concepto-sises.
+ *
+ * concepto-sises
+ * can not be copied and/or distributed without the express
+ * permission of Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
+ */
+
+namespace Concepto\Sises\ApplicationBundle\SisesApplicationBundle;
+
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SisesApplicationBundle extends Bundle
 {
-
 }
