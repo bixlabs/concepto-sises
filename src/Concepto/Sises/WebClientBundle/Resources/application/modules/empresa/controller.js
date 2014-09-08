659205114dd98702e198698fa3842ac0b7594a9d
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-09-08 17:28:29 -0500

    refactor module/empresa to new crud

---
+++ src/Concepto/Sises/WebClientBundle/Resources/application/modules/empresa/controller.js
@@ -0,0 +1,73 @@
+/**
+ * Created by julian on 8/09/14.
+ */
+;
+(function () {
+    "use strict";
+
+    /**
+     * Reused methods
+     *
+     * @param scope
+     * @constructor
+     */
+    function EmpresaListado(scope) {
+        scope.list = function() {
+            scope.go('empresas.listado');
+        };
+    }
+
+    /**
+     * EmpresaListadoController
+     *
+     * @param scope
+     * @param EmpresaFactory
+     * @constructor
+     */
+    function EmpresaListadoController(scope, EmpresaFactory) {
+        EmpresaListado.call(this, scope);
+        G.Base.ListController.call(this, scope, EmpresaFactory);
+
+        scope.add = function() {
+            scope.go('empresas.nueva');
+        };
+
+        scope.details = function(id) {
+            scope.go('empresas.detalles', {id: id});
+        };
+    }
+
+    /**
+     * EmpresaNuevaController
+     *
+     * @param scope
+     * @param EmpresaFactory
+     * @constructor
+     */
+    function EmpresaNuevaController(scope, EmpresaFactory) {
+        G.Base.NewController.call(this, scope, EmpresaFactory);
+        EmpresaListado.call(this, scope);
+    }
+
+    /**
+     * EmpresaDetallesController
+     *
+     * @param scope
+     * @param EmpresaFactory
+     * @constructor
+     */
+    function EmpresaDetallesController(scope, EmpresaFactory) {
+        G.Base.UpdateController.call(this, scope, EmpresaFactory);
+        EmpresaListado.call(this, scope);
+    }
+
+    /**
+     * Register like angular.js controllers
+     */
+    angular.module(G.modules.EMPRESA)
+        .controller('EmpresaListadoController', ['$scope', 'Empresa', EmpresaListadoController]);
+    angular.module(G.modules.EMPRESA)
+        .controller('EmpresaNuevaController', ['$scope', 'Empresa', EmpresaNuevaController]);
+    angular.module(G.modules.EMPRESA)
+        .controller('EmpresaDetallesController', ['$scope', 'Empresa', EmpresaDetallesController]);
+})();
