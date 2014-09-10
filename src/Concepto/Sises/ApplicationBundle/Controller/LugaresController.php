05d9d6167a5a95c626121b243a6671f2eb46a8ff
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-09-10 10:17:03 -0500

    lugar de entrega

---
+++ src/Concepto/Sises/ApplicationBundle/Controller/LugaresController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © 2014 Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
+ *
+ * This file is part of concepto-sises.
+ *
+ * concepto-sises
+ * can not be copied and/or distributed without the express
+ * permission of Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
+ */
+
+namespace Concepto\Sises\ApplicationBundle\Controller;
+
+
+use Concepto\Sises\ApplicationBundle\Handler\RestHandlerInterface;
+use JMS\DiExtraBundle\Annotation\LookupMethod;
+
+class LugaresController extends RestController
+{
+
+    /**
+     * @return RestHandlerInterface
+     * @LookupMethod("concepto_sises_lugar_entrega.handler")
+     */
+    public function getHandler() {}
+}
---
+++ src/Concepto/Sises/ApplicationBundle/Handler/LugarEntregaRestHandler.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © 2014 Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
+ *
+ * This file is part of concepto-sises.
+ *
+ * concepto-sises
+ * can not be copied and/or distributed without the express
+ * permission of Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
+ */
+
+namespace Concepto\Sises\ApplicationBundle\Handler;
+
+use JMS\DiExtraBundle\Annotation\Service;
+
+/**
+ * Class LugarEntregaRestHandler
+ * @package Concepto\Sises\ApplicationBundle\Handler
+ * @Service(id="concepto_sises_lugar_entrega.handler", parent="conceptos_sises_abstract_rest.handler")
+ */
+class LugarEntregaRestHandler extends RestHandler {
+
+    protected function  getTypeClassString()
+    {
+        return 'Concepto\Sises\ApplicationBundle\Form\LugarEntregaType';
+    }
+
+    protected function getOrmClassString()
+    {
+        return 'Concepto\Sises\ApplicationBundle\Entity\LugarEntrega';
+    }
+}
