53c22e3d9b4bdc67ee8b04ab5775751c0bd84f4d
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-09-12 10:42:52 -0500

    Performance: avoid load entire table

--- src/Concepto/Sises/ApplicationBundle/Form/LugarEntregaType.php
+++ src/Concepto/Sises/ApplicationBundle/Form/LugarEntregaType.php
@@ -12,21 +12,71 @@
 namespace Concepto\Sises\ApplicationBundle\Form;
 
 
+use Concepto\Sises\ApplicationBundle\Handler\RestHandlerInterface;
+use JMS\DiExtraBundle\Annotation\FormType;
+use JMS\DiExtraBundle\Annotation\Inject;
+use JMS\DiExtraBundle\Annotation\InjectParams;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+/**
+ * Class LugarEntregaType
+ * @package Concepto\Sises\ApplicationBundle\Form
+ * @FormType(alias="lugar")
+ */
 class LugarEntregaType extends AbstractType
 {
+    /**
+     * @var RestHandlerInterface
+     */
+    private $provider;
+
+    /**
+     * @param $provider
+     * @InjectParams({
+     *   "provider" = @Inject("concepto_sises_ubicacion.handler")
+     * })
+     */
+    function __construct($provider)
+    {
+        $this->provider = $provider;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('id')
             ->add('nombre')
             ->add('ubicacion', null, array(
-                'property' => 'id'
+                'property' => 'id',
+                'choices' => array()
             ))
         ;
+
+        $pr = $this->provider;
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function(FormEvent $event) use ($pr, $builder) {
+            $form = $event->getForm();
+            $data = $event->getData();
+
+            if ($form->has('ubicacion')) {
+                $form->remove('ubicacion');
+
+                $ubicacion = $pr->get($data['ubicacion']);
+
+                $formFactory = $builder->getFormFactory();
+                $form->add($formFactory->createNamed('ubicacion', 'entity', null, array(
+                    'auto_initialize' => false,
+                    'property' => 'id',
+                    'class' => 'Concepto\Sises\ApplicationBundle\Entity\Ubicacion\CentroPoblado',
+                    'choices' => $ubicacion ? array($ubicacion) : array()
+                )));
+            }
+        });
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
@@ -43,6 +93,6 @@ class LugarEntregaType extends AbstractType
      */
     public function getName()
     {
-        return '';
+        return 'lugar';
     }
 }
