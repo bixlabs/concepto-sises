97d13705a322d57432f714ca293d4e201f03ff9f
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-09-16 08:10:30 -0500

    add servicios form

--- src/Concepto/Sises/WebClientBundle/Resources/application/directive/widget/beneficios.js
+++ src/Concepto/Sises/WebClientBundle/Resources/application/directive/widget/beneficios.js
@@ -10,7 +10,44 @@
                 restricti: 'A',
                 templateUrl: G.template('directive/widget_beneficios'),
                 link: function(scope) {
+                    scope.id = G.guid();
 
+                    scope.handler = {
+                        id: scope.id,
+                        actions: {
+                            ok: {
+                                label: 'Agregar',
+                                style: 'primary'
+                            },
+                            cancel: {
+                                dismiss: true,
+                                label: 'Volver'
+                            }
+                        }
+                    };
+
+                    scope.add = function() {
+                        scope.handler.show();
+                    };
+
+                    var append = function(archivo) {
+
+                        var founded = -1;
+
+                        // Se busca por archivos, tiene nombres unicos
+                        angular.forEach(scope.elements, function(value, index) {
+                            if (value.file === archivo.file) {
+                                founded = index;
+                            }
+                        });
+
+                        if (founded < 0) {
+                            if (!scope.elements) {
+                                scope.elements = [];
+                            }
+                            scope.elements.push(archivo);
+                        }
+                    };
                 }
             };
         }])
