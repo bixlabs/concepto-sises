a8d76e56718fb1ae430747d1e90fa1bb472dccd8
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-09-25 13:18:04 -0500

    Agrega y elimina apropiadamene beneficios de un beneficiario

--- src/Concepto/Sises/WebClientBundle/Resources/application/directive/widget/beneficios.js
+++ src/Concepto/Sises/WebClientBundle/Resources/application/directive/widget/beneficios.js
@@ -4,16 +4,64 @@
 ;
 (function () {
     "use strict";
+
+    var CommonListElement = function(scope, findCallback) {
+
+        if (!scope) {
+            throw "Need pass scope as first argument!";
+        }
+
+        if (!findCallback) {
+            throw "Need define a function() to compare";
+        }
+
+        var find = function(element) {
+            var founded = -1;
+
+            // Se busca por archivos, tiene nombres unicos
+            angular.forEach(scope.elements, function(value, index) {
+                founded = findCallback.call(this, value, index, element);
+            });
+
+            return founded;
+        };
+
+        scope.removeElement = function(element, $event) {
+            $event && $event.stopPropagation();
+
+            var found = find(element);
+
+            if (found !== -1) {
+                scope.elements.splice(found, 1);
+            }
+        };
+
+        scope.addElement = function(element, $event) {
+            $event && $event.stopPropagation();
+
+            var found = find(element);
+
+
+            if (found === -1) {
+                scope.elements = scope.elements || [];
+                scope.elements.push(element);
+            }
+        }
+    };
     angular.module(G.APP)
         .directive('sisesWidgetBeneficios', ['RestResources', '$rootScope', function(RR, $r) {
             return {
                 restricti: 'A',
                 templateUrl: G.template('directive/widget_beneficios'),
+                scope: {
+                    'elements': '=sisesWidgetBeneficios'
+                },
                 link: function(scope) {
                     scope.id = G.guid();
 
                     // TODO: Debe llamarse desde crud
                     scope.contratos = RR.contrato.query();
+                    scope.element = {};
 
                     scope.handler = {
                         id: scope.id,
@@ -29,13 +77,25 @@
                         }
                     };
 
+                    CommonListElement.call(this, scope, function(value, index, element) {
+                        if (value.servicio === element.servicio && value.lugar === element.lugar) {
+                            return index;
+                        }
+                    });
+
                     $r.$on('modalize.action.' + scope.id, function(event, data) {
                         switch (data) {
                             case "ok":
-                                console.log(scope.element);
+                                // Contrato no es necesario
+                                if (scope.element['contrato']) {
+                                    delete scope.element['contrato'];
+                                }
+
+                                scope.addElement(scope.element);
+                                scope.handler.hide();
                                 break;
                             default:
-                                throw 'Respuesta no capturada' + data;
+                                break;
                         }
                     });
 
