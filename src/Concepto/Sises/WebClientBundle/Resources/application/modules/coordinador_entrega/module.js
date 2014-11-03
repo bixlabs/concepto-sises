fc560feb9c7dbbccbe5b2f032c36542e4c3b5c1b
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-11-03 09:32:19 -0500

    muestra el listado de beneficiarios por entrega y pagina

--- src/Concepto/Sises/WebClientBundle/Resources/application/modules/coordinador_entrega/module.js
+++ src/Concepto/Sises/WebClientBundle/Resources/application/modules/coordinador_entrega/module.js
@@ -34,14 +34,49 @@
                 now: null
             };
 
+            scope.pagination = {
+                items: [],
+                total: 0,
+                max: 25,
+                page: 1
+            };
+
+            scope.listado = [];
+            scope.entregas = {};
+
+            // Devuelve el numero para el registro mostrado
+            scope.getNumber = function getNumber(index) {
+                var p = scope.pagination;
+
+                return ((p.page-1) * p.max) + index + 1;
+            };
+
             // Carga la asignacion
             scope.$watch('seleccion.asignacion_id', function cargaAsignacion(id) {
                 if (id) {
                     scope.seleccion.asignacion = RR.coordinador_entrega.get({'id': id});
+                    scope.listado = [];
                 }
             });
 
-            //
+            scope.$watch('listado', function(items) {
+                scope.pagination.page = 1;
+                scope.updatePagination();
+            });
+
+            scope.updatePagination = function updatePagination() {
+                var p = scope.pagination,
+                    offset = (p.page -1) * p.max,
+                    end;
+
+                scope.pagination.total = scope.listado.length;
+
+                end = Math.min(scope.pagination.total, offset + p.max);
+                scope.pagination.items =
+                    scope.listado.length > 0 ? scope.listado.slice(offset, end): [];
+            };
+
+            // Actualiza el listado al seleccionar fecha
             scope.$watch('seleccion.now', function(now) {
                 if (now) {
                     var id = scope.seleccion.asignacion.id;
@@ -50,6 +85,7 @@
                         fecha: moment(now).format('YYYY-MM-DDTHH:mm:ssZZ')
                     }).success(function(data) {
                         scope.seleccion.asignacion = RR.coordinador_entrega.get({'id': id});
+                        scope.listado = data;
                     });
                 }
             });
