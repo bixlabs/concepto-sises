53074217fd263e4a64330aaf21038060104f12a4
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-11-12 08:23:09 -0500

    agrega la funcion date

---
+++ src/Concepto/Sises/ApplicationBundle/Query/Functions/DateFunction.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright © 2014 Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
+ *
+ * This file is part of concepto-sises.
+ *
+ * concepto-sises
+ * can not be copied and/or distributed without the express
+ * permission of Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
+ */
+
+namespace Concepto\Sises\ApplicationBundle\Query\Functions;
+
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class DateFunction extends FunctionNode
+{
+    private $arg;
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf('DATE(%s)', $this->arg->dispatch($sqlWalker));
+    }
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->arg = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
