d69a4e6e66cf7326d0aa30c2324043ce1ca3001e
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-11-13 15:30:27 -0500

    agregados mensajes toast en case de exito o falla al procesar peticiones

--- src/Concepto/Sises/WebClientBundle/Resources/application/app.js
+++ src/Concepto/Sises/WebClientBundle/Resources/application/app.js
@@ -9,29 +9,70 @@
         'ui.router',
         'localytics.directives',
         'plupload.directive',
-        'ui.bootstrap'
+        'ui.bootstrap',
+        'ngToast'
     ];
 
     angular.module(G.APP, M.concat(modules))
-        .config(['plUploadServiceProvider', '$provide', function (plUploadServiceProvider, $provide) {
-            //plUploadServiceProvider.setConfig('flashPath', 'bower_components/plupload-angular-directive/plupload.flash.swf');
-            //plUploadServiceProvider.setConfig('silverLightPath', 'bower_components/plupload-angular-directive/plupload.silverlight.xap');
-            plUploadServiceProvider.setConfig('uploadPath', G.route('_uploader_upload_documentable'));
+        .factory('sisesToastOnError', ['$q', 'ngToast', function($q, ngToast) {
+            return {
+                response: function(response) {
+                    if (response.config && response.config.method) {
 
-            // Format monkeypatch angular ui bootstrap datepicker
-            $provide.decorator('dateParser', function($delegate){
+                        switch (response.config.method) {
+                            case "POST":
+                            case "PUT":
+                                ngToast.create({
+                                    'content': '<i class="glyphicon glyphicon-ok"></i> Peticion procesada',
+                                    'class': 'success',
+                                    'verticalPosition': 'top',
+                                    'horizontalPosition': 'center'
+                                });
+                                break;
+                            default:
+                                break;
+                        }
 
-                var oldParse = $delegate.parse;
-                $delegate.parse = function(input, format) {
-                    if ( !angular.isString(input) || !format ) {
-                        return input;
                     }
-                    return oldParse.apply(this, arguments);
-                };
+                    return response || $q.when(response);
+                },
+                responseError: function(response) {
+                    ngToast.create({
+                        'content': '<i class="glyphicon glyphicon-remove"></i> Ha ocurrido un error y no se pudo procesar la informacion',
+                        'class': 'danger',
+                        'verticalPosition': 'top',
+                        'horizontalPosition': 'center'
+                    });
 
-                return $delegate;
-            });
+                    window.toast = ngToast;
+
+                    return $q.reject(response);
+                }
+            };
         }])
+        .config([
+            'plUploadServiceProvider', '$provide', '$httpProvider',
+            function (plUploadServiceProvider, $provide, $httpProvider) {
+                //plUploadServiceProvider.setConfig('flashPath', 'bower_components/plupload-angular-directive/plupload.flash.swf');
+                //plUploadServiceProvider.setConfig('silverLightPath', 'bower_components/plupload-angular-directive/plupload.silverlight.xap');
+                plUploadServiceProvider.setConfig('uploadPath', G.route('_uploader_upload_documentable'));
+
+                // Format monkeypatch angular ui bootstrap datepicker
+                $provide.decorator('dateParser', function($delegate){
+
+                    var oldParse = $delegate.parse;
+                    $delegate.parse = function(input, format) {
+                        if ( !angular.isString(input) || !format ) {
+                            return input;
+                        }
+                        return oldParse.apply(this, arguments);
+                    };
+
+                    return $delegate;
+                });
+
+                $httpProvider.interceptors.push('sisesToastOnError');
+            }])
         .run([
             '$rootScope', '$state', '$stateParams', 'modalService', 'MenuService',
             function ($r, $state, $sP, mS, MS) {
