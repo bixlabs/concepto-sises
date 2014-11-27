8e8f0b9d9a7e75d8e17c6fcc43dbd286f52e05a8
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2014-11-27 08:58:56 -0500

    Entregas futuras no estan disponibles para realizar entregas

--- src/Concepto/Sises/ApplicationBundle/Entity/Entrega/EntregaAsignacionRepository.php
+++ src/Concepto/Sises/ApplicationBundle/Entity/Entrega/EntregaAsignacionRepository.php
@@ -27,6 +27,9 @@ class EntregaAsignacionRepository extends EntityRepository
         // Se asegura que no se vean entregas despues de la fecha de cierre + dias de gracia
         $qb->andWhere("CURRENT_DATE() <= DATE_ADD({$alias}.fechaCierre, {$alias}.diasGracia, 'day')");
 
+        // Se asegura que no se vean entregas antes de la fecha para que fueron creadas
+        $qb->andWhere("{$alias}.fechaInicio < CURRENT_DATE()");
+
         return $qb;
     }
 
